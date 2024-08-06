17dc9d922a4e48f1bde9cc7c137c7ab0dde6f94c
KayMas2808 <sammyakjain2808@gmail.com>
2024-08-06 10:14:30 +0530

    Initial Login Page base

--- src/atm/user/pkginterface/with/backend/Login.java
+++ src/atm/user/pkginterface/with/backend/Login.java
@@ -1,8 +1,16 @@
 package atm.user.pkginterface.with.backend;
+import javax.swing.*;
 
-public class Login {
+public class Login extends JFrame{
+    Login(){
+        setTitle("Automated Teller Machine");
+        setSize(800,480);//frame dimensions
+        setVisible(true);//make it visible
+        setLocation(350,200);//set its location on the screen(x,y)
+    }
 
     public static void main(String args[]) {
+        new Login();
         
     }
 }
