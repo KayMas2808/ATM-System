c96106649a0587602caa84d585ac43fa9d28c891
KayMas2808 <sammyakjain2808@gmail.com>
2024-08-06 12:59:54 +0530

    Login page buttons added with clear functionality.

--- src/atm/user/pkginterface/with/backend/Login.java
+++ src/atm/user/pkginterface/with/backend/Login.java
@@ -1,8 +1,13 @@
 package atm.user.pkginterface.with.backend;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.*;
 
-public class Login extends JFrame{
+public class Login extends JFrame implements ActionListener{
+    
+    JButton login, clear, signUp;
+    JTextField cardNoTxtField;
+    JPasswordField pinTxtField;
     Login(){
         setTitle("Automated Teller Machine");
         
@@ -12,21 +17,22 @@ public class Login extends JFrame{
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
         JLabel label = new JLabel(i3);//only image icon can go into jlabel
-        label.setBounds(70,10,100,100);
+        label.setBounds(90,10,100,100);
         add(label);//place object on frame using add
         
         JLabel text = new JLabel("Welcome to ATM");
         text.setFont(new Font("Osward", Font.BOLD,38));
-        text.setBounds(200,40,400,30);
+        text.setBounds(250,40,400,30);
         add(text);
         
-        JLabel cardno = new JLabel("Card Number:");
+        JLabel cardno = new JLabel("Card Num.:");
         cardno.setFont(new Font("Raleway", Font.BOLD,28));
-        cardno.setBounds(120,150,250,30);
+        cardno.setBounds(120,150,210,30);
         add(cardno);
         
-        JTextField cardNoTxtField = new JTextField();//for taking text input
-        cardNoTxtField.setBounds(310,150,250,30);
+        cardNoTxtField = new JTextField();//for taking text input
+        cardNoTxtField.setBounds(300,150,230,30);
+        cardNoTxtField.setFont(new Font("Raleway", Font.PLAIN, 14));
         add(cardNoTxtField);
         
         JLabel pin = new JLabel("Pin:");
@@ -34,15 +40,50 @@ public class Login extends JFrame{
         pin.setBounds(120,220,150,30);
         add(pin);
         
-        JTextField pinTxtField = new JTextField();//for taking text input
-        pinTxtField.setBounds(310,220,250,30);
+        pinTxtField = new JPasswordField();//for taking text input
+        pinTxtField.setBounds(300,220,230,30);
+        pinTxtField.setFont(new Font("Raleway", Font.PLAIN, 14));
         add(pinTxtField);
+        
+        login = new JButton("Sign In");
+        login.setBounds(300,300,100,30);
+        login.setBackground(Color.BLACK);
+        login.setForeground(Color.WHITE);
+        login.addActionListener(this);
+        add(login);
+        
+        clear = new JButton("Clear");
+        clear.setBounds(430,300,100,30);
+        clear.setBackground(Color.BLACK);
+        clear.setForeground(Color.WHITE);
+        clear.addActionListener(this);
+        add(clear);
+        
+        signUp = new JButton("Sign Up");
+        signUp.setBounds(300,350,230,30);
+        signUp.setBackground(Color.BLACK);
+        signUp.setForeground(Color.WHITE);
+        signUp.addActionListener(this);
+        add(signUp);
         
         getContentPane().setBackground(Color.WHITE);
         
         setSize(800,480);//frame dimensions
         setVisible(true);//make it visible
         setLocation(200,100);//set its location on the screen(x,y)
+    }
+    
+    public void actionPerformed(ActionEvent ae){
+        if (ae.getSource() == clear){
+            cardNoTxtField.setText("");
+            pinTxtField.setText("");
+        }
+        else if (ae.getSource() == login){
+            
+        }
+        else if (ae.getSource() == signUp){
+            
+        }
     }
 
     public static void main(String args[]) {
